1ab733f6e8171ef2f8fbb111da77e6cbed2b83e0
TomiAltero <tomasaltero@gmail.com>
2023-06-01 23:50:09 -0300

    asistente ejecutable terminado

--- assistant/assistant.py
+++ assistant/assistant.py
@@ -5,23 +5,41 @@ from reproduccion_yt import reproducir_youtube
 from capturar_audio import obtener_audio, reconocer_texto
 from obtener_clima import obtener_clima
 from crear_alarma import establecer_alarma
+from chistes import chistes
+from whatsapp import mensaje_whatsapp
 
 
 def main():
-    audio = obtener_audio()
-    texto = reconocer_texto(audio)
+    while True:
+        audio = obtener_audio()
+        texto = reconocer_texto(audio)
 
-    if texto is not None:
-        if "Reproduce" in texto or "reproduce" in texto:
-            reproducir_youtube(texto)
-        elif "busca" in texto or "Busca" in texto:
-            buscar_en_internet(texto)
-        elif "clima" in texto:
-            obtener_clima(texto)
-        elif "alarma" in texto:
-            establecer_alarma(texto)
+        if texto is not None:
+            if "Reproduce" in texto or "reproduce" in texto:
+                reproducir_youtube(texto)
+            elif "buscar" in texto or "busca" in texto:
+                buscar_en_internet(texto)
+            elif "clima" in texto:
+                obtener_clima(texto)
+            elif "alarma" in texto:
+                establecer_alarma(texto)
+            elif "Enviar mensaje" in texto:
+                numero = input("Ingrese el numero: ")
+                mensaje = input("Ingrese el mensaje: ")
+                hora = int(input("Ingrese la hora que quieras mandar el mensaje: "))
+                minuto = int(input("Ingrese el minuto que quieras mandar el mensaje"))
+                mensaje_whatsapp(numero, mensaje, hora, minuto)
 
+            elif "Cuéntame un chiste" in texto:
+                chiste = chistes()
+                print(chiste)
+            else:
+                print("No se reconoce el comando, vuelva a intentarlo")
 
+            verif = input("Desea continuar (SI/NO): ")
+            if verif == "NO":
+                print("Nos vemos!")
+                return
 
-if __name__ == "__main__":
+if __name__ == "main":
     main()
